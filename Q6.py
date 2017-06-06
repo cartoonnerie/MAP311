d763942203f89b33c5f0d006ed5ab0b1d01934f0
cartoonnerie <melodia@melix.org>
2017-06-06 17:22:26 +0200

    passage de mp.log(x,2) à np.log2(x) \n compactage des fonctions Hx et Hxy

--- Q6.py
+++ Q6.py
@@ -7,7 +7,6 @@ Created on Fri Jun  2 21:38:11 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
-import mpmath as mp
 
 #Q6a
 
@@ -16,23 +15,15 @@ Px = np.array([14/16, 1/16, 1/16])
 
 #entropie d'une variable aléatoire
 def Hx(P):
-    somme = 0.
-    for p in P:
-        if p != 0.: 
-        #on ne fait rien si p = 0 (convention)
-            somme += -p * mp.log(p, 2)
-    return somme
+   return -sum([p*np.log2(p) for p in P if p != 0])
+
 
 #loi de Y|X sous forme de tableau Pxy[0] = P_Y|(X = x0)
 Pxy = np.array([[1., 0.], [0., 1.],[0., 1.]])
 
 def Hxy(Px, P): #len(Px) = len(P)
-    somme = 0.
     n = len(P)
-    for i in range(n):
-        somme += Px[i] * Hx(P[i])
-    return somme
-
+    return sum([Px[i] * Hx2(P[i]) for i in range(n)])
 
 limit = Hxy(Px, Pxy)
 
