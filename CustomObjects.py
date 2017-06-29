cfa09c99ee31e4a7dd0728c9bf9579aca82a19c3
Riernar <riernar@users.noreply.github.com>
2017-06-29 15:25:27 +0200

    Question 9c codee : creation de l'arbre de huffman, via une methode pythonique. Vive les objets abstraits !

--- CustomObjects.py
+++ CustomObjects.py
@@ -3,30 +3,79 @@
 
 
 class Token:
+    """A class used to redifine the object used for comparison for
+    other classes"""
     def __init__(self, value, comparison_value) :
-        self.value = value
-        self.comp = comparison_value
+        self.__value = value
+        self.__comp = comparison_value
         pass
     
+    ##Redifining all comparison to the object to use for comparison
     def __lt__(self, other):
-        return self.comp < other
+        return self.__comp < other
     
     def __le__(self, other):
-        return self.comp <= other
+        return self.__comp <= other
     
     def __eq__(self, other):
-        return self.comp == other
+        return self.__comp == other
     
     def __ge__(self, other):
-        return self.comp >= other
+        return self.__comp >= other
     
     def __gt__(self, other):
-        return self > other
+        return self.__comp > other
+    
+    ##redirecting all attribute access, functioncal, representation and so on
+    ##to the original object
+    def __str__(self):
+        return self.__value.__str__()
+        
+    def __repr__(self):
+        return self.__value.__repr__()
+        
+    def __getattr__(self, name):
+        return self.__value.__getattribute__(name)
+    
+    def __hash__(self, *args):
+        return self.__value.__hash__(*args)
+        
+        
+        
+class HuffTree:
+    """Used to construct a Huffman Tree"""
+    def __init__(self, value, childs=None):
+        self.value = value
+        if childs :
+            self.left, self.right = childs
+            self.__isLeaf = False
+        else :
+            self.__isLeaf = True #This particular tree is a leaf
+            self.left, self.right = None, None
+        pass
+    
+    def toDict(self, prefix=None) :
+        """Recursively convert the whole tree to a dict representation
+        with keys beeing the encoded word and values being the huffman code for that word"""
+        #Prefix represents the first part of the coding, with is given by the parent tree
+        if prefix == None:
+            #No parent tree, calling with no prefix
+            return self.toDict('')
+            
+        if self.__isLeaf:
+            return {self.value: prefix}
+        else:
+            rep = {}
+            rep.update(self.left.toDict(prefix + '0'))
+            rep.update(self.right.toDict(prefix + '1'))
+            return rep
         
     def __str__(self):
-        return self.value.__str__()
-        
-    def __rep__(self):
-        return self.value.__rep__()
+        return '[{value}, {left}, {right}]'.format(self)
     
+    def __repr__(self):
+        return 'HuffTree(%s, %s, %s)' % (self.value.__repr__(),
+            self.left.__repr__(),
+            self.right.__repr__())
+            
         
--- Q9.py
+++ Q9.py
@@ -20,52 +20,56 @@ def occurencies(s):
     pass
 
 def huffman(s):
+    #Creation du tas
     tas = []
+    #Creation du dictionnaire des occurences
     occ = occurencies(s)
+    #Initialisation du tas :
+    #chaque lettre est ajoutée une fois,
+    #sous la forme d'une feuille d'un arbre de Huffman
+    #La classe Token redéfinie les comparaisons afin de pouvoir utiliser
+    #le module heapq sans recoder les tas.
     for char in set(s) :
-        heapq.heappush(tas, Token(char, occ[char]))
-        
+        heapq.heappush(tas, Token(HuffTree(char), occ[char]))
+    
+    #Fonction auxiliaire récursive        
     def aux(tas):
-        
+        #extraction des des plus petits éléments
+        small = heapq.heappop(tas)
+        big = heapq.heappop(tas)
+        if len(tas) == 0:
+            #si le tas ne contenait que deux elements, on a atteint la dernière etape
+            return HuffTree(small.value + big.value, (small, big))
+        else :
+            #s'il reste des elements, alors :
+            #  - On construit l'arbre de huffman des deux plus petit elements:
+            #  - On lui donne une probabilité égale à la somme des probabilités
+            #    des elements qu'il represente
+            #  - On continue la construction de l'arbre
+            heapq.heappush(tas, Token(HuffTree(small.value + big.value,
+                                               (small,big)),
+                                      small._Token__comp + big._Token__comp))
+            return aux(tas)
+    #la fonction récursive construit un HuffTree, il n'y a plus qu'a le convertir
+    #sous une forme plus utile pour l'encodage, un dictionnaire.
+    return aux(tas).toDict('')
 
-
-for i in sunit :
-    heapq.heappush(tas, Char(i))
-    pass
-print(tas)
-
-def tasOccurences(s):
-    sunit = set(s)
-    tas = []
-    for i in sunit :
-        heapq.heappush(tas, Token(i, ))
-    return tas
-
-def huffman(s): #tentative non aboutie
-
-    tas = []
-    Char = InitTokenClass(occurencies(s))
-    for i in sunit :
-        heapq.heappush(tas, Char(i))
-    res = {}
     
-    
-
 #questionD
 
 def genererMot(size, chars): 
 #https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python
     return ''.join(random.choice(chars) for _ in range(size))
 
-longueur = 13
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
-d = len(alphabet)
-texte = id_generator(longueur, alphabet)
-print(texte)
-#codage = huffman(texte) #attendu un dicttionnaire de la forme {char : codage}
-freq = np.array([x for x in occurencies(texte).values()])
-freq = freq / longueur
-entropie = Q6.Hx(freq)
+#longueur = 13
+#alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+#d = len(alphabet)
+##texte = id_generator(longueur, alphabet)
+#print(texte)
+##codage = huffman(texte) #attendu un dicttionnaire de la forme {char : codage}
+#freq = np.array([x for x in occurencies(texte).values()])
+#freq = freq / longueur
+#entropie = Q6.Hx(freq)
 
 #avg = np.average(np.array([x for x in codage.values()]))
 #print((avg, entropie/np.log2(d) + 1))
