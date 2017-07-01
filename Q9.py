6c13bd7c8c2de924dda8386e0b43341c0c820945
Alice <melodia@melix.org>
2017-07-01 15:18:26 +0200

    Q9d et e sur Python et latex. J'ai aussi faut une variante de la fonction huffman qui rend le codage à partir des fréquences des caractères

--- Q9.py
+++ Q9.py
@@ -11,7 +11,7 @@ from CustomObjects import Token, HuffTree
 
 import Q6
 
-
+#Question B
 def occurencies(s):
     rep = {}
     for char in s :
@@ -19,17 +19,16 @@ def occurencies(s):
     return rep
     pass
 
-def huffman(s):
+#Question C
+def huffmanAux(occ, alphabetSet):
     #Creation du tas
     tas = []
-    #Creation du dictionnaire des occurences
-    occ = occurencies(s)
     #Initialisation du tas :
     #chaque lettre est ajoutée une fois,
     #sous la forme d'une feuille d'un arbre de Huffman
     #La classe Token redéfinie les comparaisons afin de pouvoir utiliser
     #le module heapq sans recoder les tas.
-    for char in set(s) :
+    for char in alphabetSet :
         heapq.heappush(tas, Token(HuffTree(char), occ[char]))
     
     #Fonction auxiliaire récursive        
@@ -54,6 +53,12 @@ def huffman(s):
     #sous une forme plus utile pour l'encodage, un dictionnaire.
     return aux(tas).toDict('')
 
+def huffman(texte):
+    #Creation du dictionnaire des occurences
+    occ = occurencies(texte)
+    return huffmanAux(occ, set(texte))
+    
+    
     
 #questionD
 
@@ -61,16 +66,28 @@ def genererMot(size, chars):
 #https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python
     return ''.join(random.choice(chars) for _ in range(size))
 
-longueur = 26
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
-d = len(alphabet)
+d = 2
+
+#codage par rapport à une chaine de caractères uniformément répartis
+longueur = 9999
 texte = genererMot(longueur, alphabet)
-print(texte)
-codage = huffman(texte) #attendu un dicttionnaire de la forme {char : codage}
+codage = huffman(texte) #attendu un dictionnaire de la forme {char : codage}
 freq = np.array([x for x in occurencies(texte).values()])
 freq = freq / longueur
-#entropie = Q6.Hx(freq)
+entropie = Q6.Hx(freq)
+avg = np.sum(np.array([freq[i]*len(codage[alphabet[i]]) for i in range(len(alphabet))]))
 
-#avg = np.average(np.array([x for x in codage.values()]))
-#print((avg, entropie/np.log2(d) + 1))
+#codage par rapport aux fréquences des caractères en français
+freqFrancais = np.array([67563628, 10817171, 30219574, 34914685, 115024205, 10579192, 11684140, 10583562, 62672992, 3276064, 2747547])
+freqFrancais = freqFrancais/np.sum(freqFrancais)
+occFr = {alphabet[i] : freqFrancais[i] for i in range(len(alphabet))}
+codageFranc = huffmanAux(occFr, set(alphabet))
+entropieFr = Q6.Hx(freqFrancais)
+avgFranc = np.sum(np.array([freqFrancais[i]*len(codageFranc[alphabet[i]]) for i in range(len(alphabet))]))
+
+print("pour une probabilité uniforme : ", avg, (entropie/np.log2(d)) + 1)
+print("pour les fréquences du français : ", avgFranc, (entropieFr/np.log2(d)) + 1)
         
